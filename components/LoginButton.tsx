e23fbc1c0996e602b5f2613d9665de33630691ef
hvKalista <alonzo.sabino814@gmail.com>
2023-07-13 20:45:00 -0500

    auth fix

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -9,7 +9,7 @@ export function LoginButton() {
     await supabase.auth.signInWithOAuth({
       provider: 'discord',
       options: {
-        redirectTo: `${process.env.NEXT_PUBLIC_URL!}/api/auth/callback`,
+        redirectTo: `${process.env.NEXT_PUBLIC_URL!}/auth/callback`,
         scopes: 'identify guilds'
       }
     })
