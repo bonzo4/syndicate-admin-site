1f73abab607de379f73f386388942ad9ae67621b
hvKalista <alonzo.sabino814@gmail.com>
2023-08-27 18:45:11 -0500

    graph resize

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import { Stats } from "@/components/stats/Stats";
 
 export default function Dashboard() {
   return (
-    <div className="animate-in flex flex-col items-center opacity-0 px-3 text-foreground grow w-[80%]">
+    <div className="animate-in flex flex-col items-center opacity-0 px-3 text-foreground grow w-[80%] justify-center">
         <Stats />
         <ViewsAndInteractionsGraph />
       </div>
