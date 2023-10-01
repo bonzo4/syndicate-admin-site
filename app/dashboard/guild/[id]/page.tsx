dce077cf810693dc10acdd1de122e793e6a54f19
hvKalista <alonzo.sabino814@gmail.com>
2023-10-01 18:09:30 -0500

    referral updates

--- app/dashboard/guild/[id]/page.tsx
+++ app/dashboard/guild/[id]/page.tsx
@@ -1,55 +1,66 @@
-import { GuildErrorList } from "@/components/lists/GuildErrorList"
-import { IndividualGuildStats } from "@/components/stats/IndividualGuildStats"
-import { Database } from "@/types"
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
-import { cookies } from "next/headers"
+import { GuildErrorList } from '@/components/lists/GuildErrorList';
+import { IndividualGuildStats } from '@/components/stats/IndividualGuildStats';
+import { Database } from '@/types';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
 
-export default async function GuildInfoPage({ params }: { params: { id: string } }) {
-  
-  const supabase = createServerComponentClient<Database>({ cookies })
-  
+export default async function GuildInfoPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const supabase = createServerComponentClient<Database>({ cookies });
+
   const { data: guild, error } = await supabase
     .from('guilds')
     .select('*')
     .eq('id', params.id)
     .limit(1)
-        .single()
-  
+    .single();
+
   const { data: guildSettings } = await supabase
     .from('guild_settings')
     .select('*')
     .eq('guild_id', params.id)
     .limit(1)
-        .single()
-  
+    .single();
+
   const { data: guildReferral } = await supabase
     .from('guild_referrals')
-    .select('user_id')
+    .select('discord_user_id')
     .eq('guild_id', params.id)
     .limit(1)
-        .single()
-  
-  if (error) return 
+    .single();
+
+  if (error) return;
 
   return (
-    <div className="animate-in flex flex-col items-center opacity-0 py-3 text-foreground">
-      <div className="flex flex-row space-x-2">
-        <h1 className="text-2xl font-bold text-white">
-          {guild.name}
+    <div className='animate-in flex flex-col items-center py-3 text-foreground opacity-0'>
+      <div className='flex flex-row space-x-2'>
+        <h1 className='text-2xl font-bold text-white'>{guild.name}</h1>
+        <h1 className='text-2xl font-bold text-white'>
+          {' '}
+          Setup?{guildSettings ? '✅' : '❌'}
         </h1>
-        <h1 className="text-2xl font-bold text-white"> Setup?{guildSettings ? '✅' : '❌'}</h1>
-       
-        <h1 className="text-2xl font-bold text-white">Referred?{guildReferral?.user_id ? '✅' : '❌'}</h1>
-        {guild.invite &&
-          <a className="underline hover:no-underline my-auto" href={guild.invite} target="_blank" rel="noreferrer" >
+
+        <h1 className='text-2xl font-bold text-white'>
+          Referred?{guildReferral?.discord_user_id ? '✅' : '❌'}
+        </h1>
+        {guild.invite && (
+          <a
+            className='my-auto underline hover:no-underline'
+            href={guild.invite}
+            target='_blank'
+            rel='noreferrer'
+          >
             Visit 🔗
-          </a>}
-        
+          </a>
+        )}
       </div>
       <IndividualGuildStats guildId={guild.id} members={guild.member_count} />
-      <div className="flex flex-row">
-        <div className="flex flex-col">
-          <h1 className="text-2xl font-bold text-white">Bot Errors</h1>
+      <div className='flex flex-row'>
+        <div className='flex flex-col'>
+          <h1 className='text-2xl font-bold text-white'>Bot Errors</h1>
           <GuildErrorList guildId={guild.id} />
         </div>
       </div>
